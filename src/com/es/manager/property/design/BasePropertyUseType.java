d14a32e27e3ad00e939c5c1d3342f913dc1fd9b3
Joshua Keeler <jkeeler16@gmail.com>
2015-10-01 15:47:48 -0400

    add base property type

--- src/com/es/manager/property/design/BasePropertyUseType.java
+++ src/com/es/manager/property/design/BasePropertyUseType.java
@@ -1,5 +1,16 @@
 package com.es.manager.property.design;
 
 public class BasePropertyUseType {
+	
+	private Long id;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
 }
